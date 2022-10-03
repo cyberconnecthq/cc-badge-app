4954813a36297550279f18884779b503a2404089
snowdot <co.pascu@gmail.com>
2022-10-03 17:34:43 +0300

    first commit

---
+++ src/graphql/index.ts
@@ -0,0 +1,5 @@
+export { LOGIN_GET_MESSAGE } from "./LoginGetMessage";
+export { LOGIN_VERIFY } from "./LoginVerify";
+export { CREATE_REGISTER_ESSENCE_TYPED_DATA } from "./CreateRegisterEssenceTypedData";
+export { CREATE_COLLECT_ESSENCE_TYPED_DATA } from "./CreateCollectEssenceTypedData";
+export { RELAY } from "./Relay";
