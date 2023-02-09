f852fa3277a877aa6ebf1a733c283fd28ef8a23e
Wei Su <wei@funxociety.com>
2023-02-09 18:34:21 +0800

    feat: add gas mode creating essence NFT

--- components/Buttons/BadgeBtnWithGas.tsx
+++ components/Buttons/BadgeBtnWithGas.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
-import { useMutation } from "@apollo/client";
 import { pinJSONToIPFS, getEssenceSVGData } from "../../helpers/functions";
-import { CREATE_REGISTER_ESSENCE_TYPED_DATA, RELAY } from "../../graphql";
 import { IBadgeInput, IEssenceMetadata } from "../../types";
 import { randCompanyName } from "@ngneat/falso";
 import { AuthContext } from "../../context/auth";
 import { ModalContext } from "../../context/modal";
 import { v4 as uuidv4 } from "uuid";
+import { ethers } from "ethers";
+import ProfileNFTABI from "../../abi/ProfileNFT.json";
+import { PROFILE_NFT_CONTRACT } from "../../helpers/constants";
 
 function BadgeBtn({ nftImageURL, title, venue, date }: IBadgeInput) {
 	const {
@@ -18,10 +19,6 @@ function BadgeBtn({ nftImageURL, title, venue, date }: IBadgeInput) {
 		connectWallet,
 	} = useContext(AuthContext);
 	const { handleModal } = useContext(ModalContext);
-	const [createRegisterEssenceTypedData] = useMutation(
-		CREATE_REGISTER_ESSENCE_TYPED_DATA
-	);
-	const [relay] = useMutation(RELAY);
 
 	const handleOnClick = async () => {
 		try {
@@ -86,51 +83,26 @@ function BadgeBtn({ nftImageURL, title, venue, date }: IBadgeInput) {
 			/* Get the signer from the provider */
 			const signer = provider.getSigner();
 
-			/* Get the chain id from the network */
+			const contract = new ethers.Contract(
+				PROFILE_NFT_CONTRACT,
+				ProfileNFTABI,
+				signer
+			);
 
-			/* Create typed data in a readable format */
-			const typedDataResult = await createRegisterEssenceTypedData({
-				variables: {
-					input: {
-						/* The profile id under which the Essence is registered */
-						profileID: primaryProfile.profileID,
-						/* Name of the Essence */
-						name: "Event SBT",
-						/* Symbol of the Essence */
-						symbol: "SBT",
-						/* URL for the json object containing data about content and the Essence NFT */
-						tokenURI: `https://cyberconnect.mypinata.cloud/ipfs/${ipfsHash}`,
-						/* Middleware that allows users to collect the Essence NFT for free */
-						middleware: {
-							collectFree: true,
-						},
-						/* Set if the Essence should be transferable or not */
-						/* SBTs are non-transferable */
-						transferable: false,
-					},
+			const tx = await contract.registerEssence(
+				{
+					profileId: primaryProfile?.profileID,
+					name: "gas-post",
+					symbol: "gp",
+					essenceTokenURI: ipfsHash,
+					essenceMw: "0x0000000000000000000000000000000000000000",
+					transferable: true,
+					deployAtRegister: true,
 				},
-			});
-			const typedData =
-				typedDataResult.data?.createRegisterEssenceTypedData?.typedData;
-			const message = typedData.data;
-			const typedDataID = typedData.id;
+				0x0
+			);
 
-			/* Get the signature for the message signed with the wallet */
-			const fromAddress = await signer.getAddress();
-			const params = [fromAddress, message];
-			const method = "eth_signTypedData_v4";
-			const signature = await signer.provider.send(method, params);
-
-			/* Call the relay to broadcast the transaction */
-			const relayResult = await relay({
-				variables: {
-					input: {
-						typedDataID: typedDataID,
-						signature: signature,
-					},
-				},
-			});
-			const txHash = relayResult.data?.relay?.relayTransaction?.txHash;
+			console.log("gas tx", tx);
 
 			/* Close Badge Modal */
 			handleModal(null, "");
@@ -150,10 +122,6 @@ function BadgeBtn({ nftImageURL, title, venue, date }: IBadgeInput) {
 				},
 			]);
 
-			/* Log the transaction hash */
-			console.log("~~ Tx hash ~~");
-			console.log(txHash);
-
 			/* Display success message */
 			handleModal("success", "Badge was created!");
 		} catch (error) {
@@ -168,7 +136,7 @@ function BadgeBtn({ nftImageURL, title, venue, date }: IBadgeInput) {
 
 	return (
 		<button className="badge-btn center" type="submit" onClick={handleOnClick}>
-			Badge
+			Badge (Gas mode)
 		</button>
 	);
 }
--- components/Forms/BadgeForm.tsx
+++ components/Forms/BadgeForm.tsx
@@ -1,68 +1,86 @@
 import { useState, ChangeEvent } from "react";
 import { IBadgeInput } from "../../types";
-import BadgeBtn from "../Buttons/BadgeBtn";
+import BadgeBtnWithGas from "../Buttons/BadgeBtnWithGas";
+import BadgeBtnWithGasless from "../Buttons/BadgeBtnWithGasless";
 
 const BadgeForm = () => {
-    const [badgeInput, setBadgeInput] = useState<IBadgeInput>({
-        nftImageURL: "",
-        title: "",
-        venue: "twitter",
-        date: Number(Date.now()),
+  const [badgeInput, setBadgeInput] = useState<IBadgeInput>({
+    nftImageURL: "",
+    title: "",
+    venue: "twitter",
+    date: Number(Date.now()),
+  });
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setBadgeInput({
+      ...badgeInput,
+      [name]: name === "date" ? new Date(value).getTime() : value,
     });
+  };
 
-    const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setBadgeInput({
-            ...badgeInput,
-            [name]: name === "date" ? new Date(value).getTime() : value
-        });
-    };
-
-    return (
-        <div className="form badge-form">
-            <h2>Create badge</h2>
-            <div>
-                <label>NFT image url</label>
-                <input
-                    name="nftImageURL"
-                    value={badgeInput.nftImageURL}
-                    onChange={handleOnChange}
-                    placeholder="https://"
-                ></input>
-            </div>
-            <div>
-                <label>Event name</label>
-                <input
-                    name="title"
-                    value={badgeInput.title}
-                    onChange={handleOnChange}
-                ></input>
-            </div>
-            <div>
-                <label>Event date</label>
-                <input
-                    name="date"
-                    value={new Date(badgeInput.date).toISOString().split("T")[0]}
-                    onChange={handleOnChange}
-                    type="date"
-                ></input>
-            </div>
-            <div className="form-badge-venue">
-                <div>Event venue</div>
-                <div>
-                    <label>Twitter
-                        <input type="radio" name="venue" value="twitter" defaultChecked onChange={handleOnChange} />
-                    </label>
-                    <label>Discord
-                        <input type="radio" name="venue" value="discord" onChange={handleOnChange} />
-                    </label>
-                </div>
-            </div>
-            <div className="form-note"><strong>Note:</strong> For empty fields we will randomly generate values.</div>
-            <BadgeBtn {...badgeInput} />
+  return (
+    <div className="form badge-form">
+      <h2>Create badge</h2>
+      <div>
+        <label>NFT image url</label>
+        <input
+          name="nftImageURL"
+          value={badgeInput.nftImageURL}
+          onChange={handleOnChange}
+          placeholder="https://"
+        ></input>
+      </div>
+      <div>
+        <label>Event name</label>
+        <input
+          name="title"
+          value={badgeInput.title}
+          onChange={handleOnChange}
+        ></input>
+      </div>
+      <div>
+        <label>Event date</label>
+        <input
+          name="date"
+          value={new Date(badgeInput.date).toISOString().split("T")[0]}
+          onChange={handleOnChange}
+          type="date"
+        ></input>
+      </div>
+      <div className="form-badge-venue">
+        <div>Event venue</div>
+        <div>
+          <label>
+            Twitter
+            <input
+              type="radio"
+              name="venue"
+              value="twitter"
+              defaultChecked
+              onChange={handleOnChange}
+            />
+          </label>
+          <label>
+            Discord
+            <input
+              type="radio"
+              name="venue"
+              value="discord"
+              onChange={handleOnChange}
+            />
+          </label>
         </div>
-    );
+      </div>
+      <div className="form-note">
+        <strong>Note:</strong> For empty fields we will randomly generate
+        values.
+      </div>
+      <BadgeBtnWithGasless {...badgeInput} />
+      <BadgeBtnWithGas {...badgeInput} />
+    </div>
+  );
 };
 
 export default BadgeForm;
